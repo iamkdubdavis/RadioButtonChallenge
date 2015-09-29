3ecbcbccc3785de2a693746965192308015f774e
Kimberly Davis <kwatsondavis@gmail.com>
2015-09-29 08:58:55 -0400

    Initial Commit

---
+++ ChallengeConditionalRadioButton/Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeConditionalRadioButton
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            resultLabel.Text = " ";
+
+            if (pencilRadioButton.Checked)
+            {
+                resultImage.ImageUrl = "pencil.png";
+                resultLabel.Text = "You selected Pencil";
+            }
+            else if (penRadioButton.Checked)
+            {
+                resultImage.ImageUrl = "pen.png";
+                resultLabel.Text = "You selected Pen";
+            }
+            else if (phoneRadioButton.Checked)
+            {
+                resultImage.ImageUrl = "phone.png";
+                resultLabel.Text = "You selected Phone";
+            }
+            else if (tabletRadioButton.Checked)
+            {
+                resultImage.ImageUrl = "tablet.png";
+                resultLabel.Text = "You selected Tablet";
+            }
+            else
+                resultLabel.Text = "Please select an option";
+        }
+    }
+}
